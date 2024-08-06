38b9b5bfb9c6f76277b2c3be2f31ab3a60ca0205
lassc123 <3096079594@qq.com>
2024-08-06 09:20:11 +0800

    2024/8/6

---
+++ algorithm/Data Structure/array_stack.py
@@ -0,0 +1,33 @@
+class ArrayStack:
+    """基于数组实现的栈"""
+
+    def __init__(self):
+        """构造方法"""
+        self._stack:list[int] = []
+
+    def size(self)->int:
+        """获取栈的长度"""
+        return len(self._stack)
+    
+    def is_empty(self)->bool:
+        """判断栈是否为空"""
+        return self.siez() == 0
+    
+    def push(self,item:int):
+        """入栈"""
+        self._stack.append(item)
+
+    def pop(self)->int:
+        """出栈"""
+        if self.is_empty():
+            raise IndexError("栈为空")
+        
+    def peek(self)->int:
+        """访问栈顶元素"""
+        if self.is_empty():
+            raise IndexError("栈为空")
+        return self._stack[-1]
+    
+    def to_list(self)->list[int]:
+        """返回列表用于打印"""
+        return self._stack
