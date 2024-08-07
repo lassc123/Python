1c169ba1b4969d00c87d1aa176c3ae0102ee9a6c
lassc123 <3096079594@qq.com>
2024-08-07 22:11:59 +0800

    2024/8/7

--- Hello_algorithm/Data Structure/hasp_map_chaining.py
+++ Hello_algorithm/Data Structure/hasp_map_chaining.py
@@ -1,3 +1,9 @@
+class Pair:
+    """键值对"""
+
+    def __init__(self,key:int,val:str):
+        self.key = key
+        self.val = val
 class HashMapChaining:
     """链式地址哈希表"""
 
@@ -8,3 +14,70 @@ class HashMapChaining:
         self.load_thres = 2.0/3.0 # 触发扩容的负载因子阈值
         self.extend_ratio = 2 # 扩容倍数
         self.buckets = [[] for _ in range(self.capacity)] # 桶数组
+
+    def hash_func(self,key:int)->int:
+        """哈希函数"""
+        return key%self.capacity
+    
+    def load_factor(self)->float:
+        """负载因子"""
+        return self.size/self.capacity
+    
+    def get(self,key:int)->str|None:
+        """查询操作"""
+        index = self.hash_func(key)
+        bucket = self.buckets[index]
+        # 遍历桶，若找到key,则返回对应的val
+        for pair in bucket:
+            if pair.key ==key:
+                return pair.val
+        return None
+    
+    def put(self,key:int,val:str):
+        """添加操作"""
+        # 当负载因子超过阈值时，执行扩容
+        if self.load_factor()>self.load_thres:
+            self.extend()
+        index = self.hash_func(key)
+        bucket = self.buckets[index]
+        # 遍历桶，若遇到指定key,则更新对于val并返回
+        for pair in bucket:
+            if pair.key == key:
+                pair.val = val
+                return
+        # 若无该key，则将键值对添加至尾部
+        pair = Pair(key,val)
+        bucket.append(pair)
+        self.size += 1
+
+    def remove(self,key:int):
+        """删除操作"""
+        index = self.hash_func(key)
+        bucket = self.buckets[index]
+        # 遍历桶，从中删除键值对
+        for pair in bucket:
+            if pair.key == key:
+                bucket.remove(pair)
+                self.size -= 1
+                break
+
+    def extend(self):
+        """扩容哈希表"""
+        # 暂存原哈希表
+        buckets = self.buckets
+        # 初始化扩容后的新哈希表
+        self.capacity *= self.extend_ratio
+        self.buckets =[[] for _ in range(self.capacity)]
+        self.size = 0
+        # 将键值对从原哈希表搬运至新哈希表
+        for bucket in buckets:
+            for pair in bucket:
+                self.put(pair.key,pair.val)
+
+    def print(self):
+        """打印哈希表"""
+        for bucket in self.buckets:
+            res = []
+            for pair in bucket:
+                res.append(str(pair.key)+"->"+pair.val)
+            print(res)
