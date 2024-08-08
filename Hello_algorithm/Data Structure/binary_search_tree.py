0431358258c830c6c8b03404dbbd13f1f90eebe3
lassc123 <3096079594@qq.com>
2024-08-08 18:05:22 +0800

    2024/8/8

--- Hello_algorithm/Data Structure/binary_search_tree.py
+++ Hello_algorithm/Data Structure/binary_search_tree.py
@@ -26,3 +26,75 @@ class BinarySearchTree:
             else:
                 break
         return cur
+    
+    def insert(self,num:int):
+        """插入节点"""
+        # 若树为空，则初始化根节点
+        if self._root is None:
+            self._root = TreeNode(num)
+            return
+        # 循环查找，越过叶节点后跳出
+        cur,pre = self._root,None
+        while cur is not None:
+            # 找到重复节点，直接返回
+            if cur.val == num:
+                return
+            pre = cur
+            # 插入位置在cur的右子树中
+            if cur.val<num:
+                cur = cur.right
+            # 插入位置在cur的左子树中
+            else:
+                cur = cur.left
+        # 插入节点
+        node  = TreeNode(num)
+        if pre.val<num:
+            pre.right = node
+        else:
+            pre.left = node
+
+    def remove(self,num:int):
+        """删除节点"""
+        # 若树为空，直接提前返回
+        if self._root is None:
+            return
+        # 循环查找，越过叶节点后跳出
+        cur,pre = self._root,None
+        while cur is not None:
+            # 找到待删除节点，跳出循环
+            if cur.val == num:
+                break
+            pre =cur
+            # 待删除节点在cur的右子树中
+            if cur.val<num:
+                cur = cur.right
+            # 待删除节点在cur的右子树中
+            else:
+                cur = cur.left
+        # 若无待删除节点，则直接返回
+        if cur is None:
+            return
+        
+        # 子节点数量 = 0 or 1
+        if cur.left is None or cur.right is None:
+            # 当子节点数量 = 0/1时，child = null/该子节点
+            child = cur.left or cur.right
+            # 删除节点cur
+            if cur != self._root:
+                if pre.left == cur:
+                    pre.left = child
+                else:
+                    pre.right = child
+            else:
+                # 若删除节点为根节点，则重新指定根节点
+                self._root = child
+        # 子节点数量 = 2
+        else:
+            # 获取中序遍历中cur的下一个节点
+            tmp:TreeNode = cur.right
+            while tmp.left is not None:
+                tmp = tmp.left
+            # 递归删除节点tmp
+            self.remove(tmp.val)
+            # 用tmp覆盖cur
+            cur.val = tmp.val
